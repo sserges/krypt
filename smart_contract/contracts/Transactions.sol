eab326e1a38aa6432ce1a9c3f850ff7b40ae4dbc
sserges <agounkeserge@gmail.com>
2022-01-04 17:16:51 -0500

    transactions contract updaded

--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.0;
 
 contract Transactions {
-    uint256 transactionCounter;
+    uint256 transactionCount;
 
     event Transfer(
         address from,
@@ -21,5 +21,47 @@ contract Transactions {
         string message;
         uint256 timestamp;
         string keyword;
+    }
+
+    TransferStruct[] transactions;
+
+    function addToBlockchain(
+        address payable receiver,
+        uint256 amount,
+        string memory message,
+        string memory keyword
+    ) public {
+        transactionCount += 1;
+        transactions.push(
+            TransferStruct(
+                msg.sender,
+                receiver,
+                amount,
+                message,
+                block.timestamp,
+                keyword
+            )
+        );
+
+        emit Transfer(
+            msg.sender,
+            receiver,
+            amount,
+            message,
+            block.timestamp,
+            keyword
+        );
+    }
+
+    function getAllTransactions()
+        public
+        view
+        returns (TransferStruct[] memory)
+    {
+        return transactions;
+    }
+
+    function getTransactionCount() public view returns (uint256) {
+        return transactionCount;
     }
 }
