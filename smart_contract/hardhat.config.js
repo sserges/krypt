e1fc63ac159f47993dbe71ceb2f83993a3e91a61
sserges <agounkeserge@gmail.com>
2022-01-07 10:42:36 -0500

    TransactionContext API setup

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,4 +1,4 @@
-//
+// https://eth-ropsten.alchemyapi.io/v2/AMPOEE7CpQmjDclNFlHn8TQQlPwr6Qlb
 
 require('@nomiclabs/hardhat-waffle')
 
@@ -6,8 +6,10 @@ module.exports = {
   solidity: '0.8.0',
   networks: {
     ropsten: {
-      url: '',
-      accounts: [''],
+      url: 'https://eth-ropsten.alchemyapi.io/v2/AMPOEE7CpQmjDclNFlHn8TQQlPwr6Qlb',
+      accounts: [
+        '66cd2777bdb5de4bbcbc076f4f2a0340eb4b8bc8d008dd9114c41e5e1d1981ad',
+      ],
     },
   },
 }
